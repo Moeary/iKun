c094e246161dffc36cc5002fb7246863672a3def
Moeary <103913682+Moeary@users.noreply.github.com>
2023-10-20 21:58:22 +0800

    fix bug

--- python处理文件/bmp_image2hex_out.py
+++ python处理文件/bmp_image2hex_out.py
@@ -19,13 +19,17 @@ def binary_to_hex(binary_data):
     hex_data = hex(int(binary_data, 2))[2:]
     return hex_data.rjust(len(binary_data)//4, '0')
 
-def save_hex_data_to_file(hex_data, file_name, img_num):
-    with open(file_name, 'a') as file:  # 使用 'a' 模式以追加方式打开文件
-        file.write(f'assign cxk[{img_num-1}][32399:0] = 32400\'h{hex_data};\n')
+img_counter = 0
 
+def save_hex_data_to_file(hex_data, file_name):
+    global img_counter  # 使用全局变量
+    with open(file_name, 'a') as file:
+        file.write(f'assign cxk[{img_counter}][32399:0] = 32400\'h{hex_data};\n')
+    img_counter += 1
+    
 # 处理多张图片
-for i in range(1,1000):  # 假设你要处理1到9张图片
+for i in range(1100,1110):  # 假设你要处理1到9张图片
     image_path = f"out/frame{str(i).zfill(4)}.bmp"  # 根据图片的命名规则来拼接图片路径
     binary_data = image_to_binary(image_path) 
     hex_data = binary_to_hex(binary_data)
-    save_hex_data_to_file(hex_data, "cxk.txt", i) #保存至当前目录下的cxk.txt
+    save_hex_data_to_file(hex_data, "cxk.txt") #保存至当前目录下的cxk.txt
